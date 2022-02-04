1e60fd773a2998b1ae049ddeeae186bf88002ff1
limjiajun <2b03.1935983.limjiajun@gmail.com>
2022-02-04 08:58:47 +0800

    update

--- mainprogram.py
+++ mainprogram.py
@@ -1,3 +1,43 @@
+from stack import Stack
+from binaryTree import BinaryTree
+
+def buildParseTree(exp):
+    tokens = exp.split()
+    print(tokens)
+    stack = Stack()
+    tree = BinaryTree('?') #reference to the root node 
+    stack.push(tree)
+    currentTree = tree #reference to a node
+    for t in tokens:
+        # RULE 1: If token is '(' add a new node as left child
+        # and descend into that node
+        if t == '(':
+            currentTree.insertLeft('?')
+            stack.push(currentTree)
+            currentTree = currentTree.getLeftTree() 
+        # RULE 2: If token is operator set key of current node
+        # to that operator and add a new node as right child
+        # and descend into that node
+        elif t in ['+', '-', '*', '/']:
+            currentTree.setKey(t)
+            currentTree.insertRight('?')
+            stack.push(currentTree)
+            currentTree = currentTree.getRightTree()
+        # RULE 3: If token is number, set key of the current node
+        # to that number and return to parent
+        elif t not in ['+', '-', '*', '/', ')'] :
+            currentTree.setKey(int(t))
+            parent = stack.pop()
+            currentTree = parent
+        # RULE 4: If token is ')' go to parent of current node
+        elif t == ')':
+            currentTree = stack.pop()
+        else:
+            raise ValueError
+    return tree
+
+
+
 while True:
     print("Please select your choice ('1','2','3'):")
     print("    1. Evaluate Expression")
